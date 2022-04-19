851a254b2922f204a4a541fc6ec64e4cec6d6440
serref jaguraga <reef163@gmail.com>
2022-04-19 14:50:19 -0400

    changes

--- sweet_science_backend/client/src/Components/BoxerNews.js
+++ sweet_science_backend/client/src/Components/BoxerNews.js
@@ -3,13 +3,7 @@ import React from "react";
 function BoxerNews() {
   return (
     <div>
-      <iframe
-        src="https://www.youtube.com/embed/E7wJTI-1dvQ"
-        frameborder="0"
-        allow="autoplay; encrypted-media"
-        allowfullscreen
-        title="video"
-      />{" "}
+      <h1>Boxer News</h1>
     </div>
   );
 }
--- sweet_science_backend/client/src/Components/Tutorial.js
+++ sweet_science_backend/client/src/Components/Tutorial.js
@@ -3,7 +3,14 @@ import React from "react";
 function Tutorial() {
   return (
     <div>
-      <h1>Tutorial</h1>
+      <p> JHKJBHKJKBJBKJ</p>
+      <iframe
+        src="https://www.youtube.com/embed/E7wJTI-1dvQ"
+        frameborder="0"
+        allow="autoplay; encrypted-media"
+        allowfullscreen
+        title="video"
+      />{" "}
     </div>
   );
 }
